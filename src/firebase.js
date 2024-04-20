bdfda6731b37073ee7afdaa51d1064f532c1a954
revathi <revathipriyan18@gmail.com>
2024-04-20 14:36:22 +0530

    Redux added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAtdQhw21V2rIZIbIMnv6fhQ8wf0fgMiHo",
+    authDomain: "slack-clone-7602e.firebaseapp.com",
+    projectId: "slack-clone-7602e",
+    storageBucket: "slack-clone-7602e.appspot.com",
+    messagingSenderId: "871414678095",
+    appId: "1:871414678095:web:0d976854a489122291603a",
+    measurementId: "G-80RD4DE57D"
+  };
